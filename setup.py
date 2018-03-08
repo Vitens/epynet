80766d423dde1ef398bb74f167b4e02785fdd887
Jorn Baayen <jorn.baayen@kisters-bv.nl>
2018-03-08 16:43:29 +0100

    Install EPANET library files

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='epynet',
       author_email='abel.heinsbroek@vitens.nl',
       license='Apache Licence 2.0',
       packages=['epynet'],
+      package_data={'epynet': ['lib/*']},
       install_requires = [
           'pandas'
       ],
