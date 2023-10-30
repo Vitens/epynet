c9a1a94a1dd51b02df56d236ce9528d9c25fb6ce
BlomTbl <ton.blom@vitens.nl>
2023-10-30 09:07:01 +0100

    Init

--- epynet/__init__.py
+++ epynet/__init__.py
@@ -2,3 +2,5 @@ from .network import Network
 from .link import Link, Pipe, Pump, Valve
 from .node import Node, Junction, Reservoir, Tank
 from .objectcollection import ObjectCollection
+from .epamodule import EPA
+#from epynet.epanet import epanet
